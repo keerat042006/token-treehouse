ade8dd61a6aaf6f95bc936cf78f0d839a454aac5
Gurkeerat Singh <gurkeerat412@gmail.com>
2026-04-22 18:26:13 +0530

    remove wrong vite config location

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: '/token-treehouse/',
+base: '/',
   server: {
     host: "::",
     port: 8080,
